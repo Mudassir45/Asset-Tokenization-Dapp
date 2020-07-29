d807ada0ba6e31f54f1db1cea02800ac1064835b
Mudassir Ali <mudassirali36@gmail.com>
2020-07-30 01:32:16 +0500

    Deploying all contracts

---
+++ migrations/2_deploy_token.js
@@ -0,0 +1,14 @@
+const MyToken = artifacts.require("MyToken.sol");
+const MyTokenSale = artifacts.require("MyTokenSale");
+const MyKycContract = artifacts.require("KycContract");
+require("dotenv").config({path: "../.env"});
+
+module.exports = async function (deployer) {
+  let addr = await web3.eth.getAccounts();
+  await deployer.deploy(MyToken, process.env.INITIAL_TOKENS);
+  await deployer.deploy(MyKycContract);
+  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address, MyKycContract.address);
+  let instance = await MyToken.deployed();
+  await instance.transfer(MyTokenSale.address, process.env.INITIAL_TOKENS); // Transfering whole amount of tokens from Token owner to Crowdsale contract in
+  // order to put that tokens on sale
+};
