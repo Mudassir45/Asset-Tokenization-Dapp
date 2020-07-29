24c5e8d78ab36baab1bf25e8814b429fbcaa3877
Mudassir Ali <mudassirali36@gmail.com>
2020-07-30 01:34:24 +0500

    Truffle-Config file

---
+++ truffle-config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+require("dotenv").config({path: "./.env"});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const AccountIndex = 0;
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    development: {
+      port: 8545,
+      host: "127.0.0.1",
+      network_id: 5777
+    },
+    ganache_local: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:8545", AccountIndex)
+      },
+      network_id: 5777
+    },
+    goerli_infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/b2e9ef3a616c45f3b8b9a64ed3714848", AccountIndex)
+      },
+      network_id: 5
+    },
+    ropsten_infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/deb7e6f4e8aa4ba89dd7b507d6f27bc0", AccountIndex)
+      },
+      network_id: 3
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.6.1"
+    }
+  }
+};
